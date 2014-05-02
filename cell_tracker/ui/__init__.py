2c700646be2b5ce295c934aad3c02c7fe5acc481
Guillaume Gay <guillaume@mitotic-machine.org>
2014-05-02 13:28:49 +0200

    split ui.py  to individual modules

---
+++ cell_tracker/ui/__init__.py
@@ -0,0 +1,2 @@
+from .ipy_widgets import set_metadata
+from .qt_dialogs import get_from_excel, get_cluster
--- cell_tracker/ui/qt_dialogs.py
+++ cell_tracker/ui/qt_dialogs.py
@@ -17,9 +17,6 @@ import sys, os
 import pandas as pd
 import numpy as np
 
-from IPython.html import widgets
-from IPython.display import display, clear_output
-
 from PyQt4 import QtGui
 
 from sktracker.io import StackIO, ObjectsIO
@@ -189,77 +186,3 @@ def get_name(default=''):
     else:
         print('Keeping default name {}'.format(default))
         return default
-
-
-def set_metadata(metadata=default_metadata):
-    smw = SetMetadataWidget(metadata)
-    display(smw)
-    for child in smw.children:
-        if len(child.children) > 1:
-            child.remove_class('vbox')
-            child.add_class('hbox')
-
-
-class SetMetadataWidget(widgets.ContainerWidget):
-    ''' IPython widget to handle metadata in a user friendly way
-    '''
-
-    def __init__(self, metadata):
-        '''
-        Creates a widget to handle metadata
-        '''
-
-        self.metadata = metadata
-        children = []
-        for key, val in metadata.items():
-            child = TypeAgnosticTextWidget(key, val)
-            for grandchild in child.children:
-                grandchild.on_trait_change(self.on_value_change, 'value')
-            children.append(child)
-        self.children = children
-
-    def _update_metadata(self):
-        """
-
-        """
-        for child in self.children:
-            self.metadata[child.description] = child.value
-
-    def on_value_change(self, name, value):
-        self._update_metadata()
-        print(value)
-
-import collections
-
-class TypeAgnosticTextWidget(widgets.ContainerWidget):
-    """
-    IPython widget that parses the input values and instanciate
-    the proper textwidget according to the type.
-    """
-
-    def __init__(self, description, value):
-
-        self.description = description
-        if isinstance(value, np.float):
-            self.children = (widgets.FloatTextWidget(description=description,
-                                                     value=value),)
-        elif isinstance(value, np.int):
-            self.children = (widgets.IntTextWidget(description=description,
-                                                   value=value),)
-        elif isinstance(value, str):
-            self.children = (widgets.TextWidget(description=description,
-                                                value=value),)
-        elif isinstance(value, collections.Sequence):
-            header = widgets.LatexWidget(value=description)
-            display(header)
-            # self.children = (header,) + tuple(TypeAgnosticTextWidget(description='', value=sub_val)
-            #                                   for sub_val in value)
-            self.children = tuple(TypeAgnosticTextWidget(description='', value=sub_val)
-                                  for sub_val in value)
-
-    @property
-    def value(self):
-        if len(self.children) == 1:
-            return(self.children[0].value)
-        else:
-            return([child.value for child in self.children])
