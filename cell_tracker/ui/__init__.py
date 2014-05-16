c8f8edb51b77ea71f324f3d4927efdb25f87106a
Guillaume Gay <guillaume@mitotic-machine.org>
2014-05-16 17:59:53 +0200

    make UI libraries optional

--- cell_tracker/ui/__init__.py
+++ cell_tracker/ui/__init__.py
@@ -1,3 +1,6 @@
-from .ipy_widgets import set_metadata
-from .qt_dialogs import get_from_excel, get_cluster
-from .manual_tracking import ManualTracking, pick_border_cells
+try :
+    from .ipy_widgets import set_metadata
+    from .qt_dialogs import get_from_excel, get_cluster
+    from .manual_tracking import ManualTracking, pick_border_cells
+except ImportError:
+    print('UI elements could not be loaded, try installing IPython and PyQt4')
