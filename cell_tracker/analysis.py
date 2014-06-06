should have fixed ellipsis fit and rotation detection

cell_tracker/analysis.py
@@ -14,6 +14,12 @@ from sktracker.trajectories import Trajectories
 from sktracker.io import ObjectsIO, StackIO
 
 from .conf import defaults
+from .utils import continuous_theta
+
+import logging
+log = logging.getLogger(__name__)
+
+
 
 ellipsis_cutoffs = defaults['ellipsis_cutoffs']
 
@@ -49,22 +55,23 @@ class Ellipses():
 
     def do_fits(self, cutoffs):
 
-        param_names = ['a', 'b', 'omega', 'phi_x',
+        param_names = ['a', 'b', 'phi_x',
                        'phi_y','x0', 'y0']
         plane_components = ['r0_x', 'r0_y', 'r0_z',
                             'r1_x', 'r1_y', 'r1_z']
-        columns = ['ellipticity', 'chi2']
+        columns = ['start', 'stop', 'ellipticity', 'chi2', 'dtheta',
+                   'theta_i', 'theta_f']
         columns.extend(plane_components)
         columns.extend(param_names)
 
         self.data = pd.DataFrame(index=self.segment.index,
-                                 columns=columns)
+                                 columns=columns, dtype=np.float)
         if self.size > self.segment.shape[0]:
             warnings.warn('window size (%i) too big'
                           ' for segment with size %i\n'
                           % (self.size, self.segment.shape[0]))
             self.data['gof'] = - np.inf
-            self.data['log_radius'] = -np.inf
+            self.data['radius'] = -np.inf
             self.data['dtheta'] = 0.
             self.data['good'] =  np.zeros(self.data.shape[0])
             return self.data
@@ -74,69 +81,72 @@ class Ellipses():
 
         for i, idx in enumerate(t_stamps[:last]):
             start = idx
-            stop = t_stamps[t_stamps <= start + self.size][-1]
-            lstsq, components = fit_arc_ellipse(self.segment,
-                                                start, stop,
-                                                self.coords)
-            if lstsq is None:
-                print('''Fitting failed between {} and {} '''.format(start, stop))
-                continue
-            params = lstsq[0]
             midle = t_stamps[t_stamps <= start + self.size//2][-1]
-            a, b = params[:2]
-            a, b = np.abs(a), np.abs(b)
+            stop = t_stamps[t_stamps <= start + self.size][-1]
+            self.data.loc[midle, 'start'] = start
+            self.data.loc[midle, 'stop'] = stop
+            fit_output, components, rotated = fit_arc_ellipse(self.segment,
+                                                              start, stop, self.coords,
+                                                              return_rotated=True)
+            if fit_output[-1] not in (1, 2, 3, 4):
+                log.warning('''Fitting failed between {} and {} '''.format(start, stop))
+                log.warning(fit_output[-2])
+                continue
+            params = fit_output[0]
+            a, b, phi_x, phi_y, x0, y0 = params
+            a, b = np.abs([a, b])
             a, b = np.max((a, b)), np.min((a, b))
-            self.data.loc[midle]['ellipticity'] = a / b
+            self.data.loc[midle, 'ellipticity'] = a / b
+
             for n, p in zip(param_names, params):
                 self.data.loc[midle][n] = p
-            fvec = lstsq[2]['fvec']
-            self.data.loc[midle]['chi2'] = np.sum(fvec**2) / fvec.size
+            fvec = fit_output[2]['fvec']
+            self.data.loc[midle, 'chi2'] = np.sum(fvec**2) / fvec.size
             r0 = components[0, :]
             r1 = components[1, :]
             for n, r in zip(plane_components[:3], r0):
-                self.data.loc[midle][n] = r
+                self.data.loc[midle, n] = r
             for n, r in zip(plane_components[3:], r1):
-                self.data.loc[midle][n] = r
+                self.data.loc[midle, n] = r
+            thetas = np.arctan2(rotated.x.values - x0,
+                                rotated.y.values - x0)
+            dthetas, thetas = continuous_theta(thetas)
+            self.data.loc[midle, 'theta_i'] = thetas.min()
+            self.data.loc[midle, 'theta_f'] = thetas.max()
+            self.data.loc[midle, 'dtheta'] = thetas.ptp()
 
         self.data['gof'] = - np.log(self.data['chi2'].astype(np.float))
-        self.data['log_radius'] = np.log(
-            self.data[['a', 'b']].abs().sum(axis=1) / 2.)
-        self.data['dtheta'] = self.data['omega'] * self.size
+        self.data['radius'] = self.data[['a', 'b']].abs().sum(axis=1) / 2.
 
         self.data['good'] =  np.zeros(self.data.shape[0])
         goods = self.good_indices(cutoffs)
         if len(goods):
             self.data['good'].loc[goods] = 1.
+        # for col in columns:
+        #     self.data[col] = self.data[col].astype(np.float)
 
     def evaluate(self, idx, sampling=10):
 
-        start = idx - self.size//2
-        if start < self.data.index[0]:
-            return None
-        stop = start + self.size
-        params = self.data.loc[idx][['a', 'b', 'omega',
-                                     'phi_x', 'phi_y',
-                                     'x0', 'y0']]
-        a, b, omega, phi_x, phi_y, x0, y0 = params
-        r0 =  self.data.loc[idx][['r0_x', 'r0_y', 'r0_z']]
-        r1 =  self.data.loc[idx][['r1_x', 'r1_y', 'r1_z']]
-        r0 = np.asarray(r0)
-        r1 = np.asarray(r1)
-        n_pts = (stop - start) * sampling
-        t = np.linspace(start, stop, n_pts)
-        xs = a * np.cos(omega * t + phi_x) + x0
-        ys = b * np.sin(omega * t + phi_y) + y0
-        xs = np.vstack((xs,)*3)
-        ys = np.vstack((ys,)*3)
-        curve = r0[:, np.newaxis] * xs + r1[:, np.newaxis] * ys
-        if self.segment is not None:
-            s_center = self.segment[self.coords].loc[start:stop].mean(axis=0)
-            c_center = curve.mean(axis=1)
-            shift = s_center - c_center
-            curve[0, :] += shift[0]
-            curve[1, :] += shift[1]
-            curve[2, :] += shift[2]
-        return curve
+
+        sub_data = self.data.loc[idx]
+
+        start, stop = sub_data[['start', 'stop']].astype(np.int)
+
+        thetas = np.linspace(sub_data.theta_i,
+                             sub_data.theta_f,
+                             self.size*sampling)
+        rhos = ellipse_radius(thetas, sub_data.a, sub_data.b,
+                              sub_data.phi_x, sub_data.phi_y)
+
+        xs = rhos * np.cos(thetas) + sub_data.x0
+        ys = rhos * np.sin(thetas) + sub_data.y0
+        zs = np.zeros_like(rhos)
+
+        segdata = self.segment.loc[start:stop][self.coords]
+        pca = PCA()
+        pca.fit(segdata)
+        ellipsis_fit = pca.inverse_transform(np.vstack((xs, ys, zs)).T)
+        return ellipsis_fit
 
     def max_ellipticity(self, max_val):
         return self.data['ellipticity'].map(lambda x:
@@ -157,12 +167,12 @@ class Ellipses():
                                        np.abs(x) > min_val)
 
     def max_radius(self, max_val):
-        return self.data['log_radius'].map(lambda x:
-                                           np.exp(x) < max_val)
+        return self.data['radius'].map(lambda x:
+                                       x < max_val)
 
     def min_radius(self, min_val):
-        return self.data['log_radius'].map(lambda x:
-                                       np.exp(x) > min_val)
+        return self.data['radius'].map(lambda x:
+                                       x > min_val)
 
 
     def good_indices(self, cutoffs=ellipsis_cutoffs):
@@ -187,7 +197,7 @@ def fit_arc_ellipse(segment, start, stop,
                     return_rotated=False):
 
     pca = PCA()
-    sub_segment = segment[coords+['t']].loc[start:stop]
+    sub_segment = segment[coords].loc[start:stop]
     if sub_segment.shape[0] < 4:
         warnings.warn('''Not enough points to fit an ellipse''')
         raise ValueError('''Not enough points to fit an ellipse''')
@@ -197,70 +207,47 @@ def fit_arc_ellipse(segment, start, stop,
     components = pca.components_
     to_fit = pd.DataFrame(data=rotated, index=sub_segment.index,
                           columns=('x', 'y', 'z'))
-    to_fit['t'] = sub_segment['t']
     # initial guesses
     a0 = to_fit['x'].ptp()
     b0 = to_fit['y'].ptp()
-
-    thetas = np.arctan2(to_fit['y'], to_fit['y'])
-    dtheta0 = thetas.iloc[-1] - thetas.iloc[0]
-    t_span = to_fit['t'].iloc[-1] - to_fit['t'].iloc[0]
-
-    omega0 = dtheta0 / t_span
-    phi_x0 = phi_y0 = 0.
-    x00, y00 = to_fit.max(axis=0)[['x', 'y']]
-
-    params0 = a0, b0, omega0, phi_x0, phi_y0, x00, y00
-    lstsq = leastsq(arc_residuals, params0, args=(to_fit, ), full_output=1)
+    phi_x0 , phi_y0 = 0., 0.
+    x00, y00 = 0, 0
+    params0 = a0, b0, phi_x0, phi_y0, x00, y00
+    fit_output = leastsq(residuals, params0, [to_fit.x, to_fit.y],
+                         full_output=1)
     if return_rotated:
-        return lstsq, components, to_fit
+        return fit_output, components, to_fit
     else:
-        return lstsq, components
+        return fit_output, components
 
-def arc_ellipse(t, params):
 
-    a, b, omega, phi_x, phi_y, x0, y0 = params
-    #a, b, omega, phi_x, phi_y = params
+def ellipse_radius(thetas, a, b, phi_x, phi_y):
+    ''' Computes the radius of an ellipse with
+    the given paramters for the angles given by `thetas`
 
-    x = a * np.cos(omega*t + phi_x) + x0
-    y = b * np.sin(omega*t + phi_y) + y0
-    return x, y
+    Paramters
+    ---------
+    thetas: ndarray
+        Angles in radians, in a polar coordiante system) for which the
+        ellipses radius is to be computed
+    a, b: floats
+        The major and minor radii of the ellipsis
+    phi_x, phi_y: floats
+        Ellipsis phases along x and y, respectively
+    '''
+    rhos = a * b / np.sqrt((b * np.cos(thetas + phi_x))**2
+                           + (a * np.sin(thetas + phi_y))**2)
 
-def arc_residuals(params, data):
+    return rhos
 
-    times = data['t'].values
-    fit_x, fit_y = arc_ellipse(times, params)
-    res_x = fit_x - data['x'].values
-    res_y = fit_y - data['y'].values
-    return np.hstack((res_y, res_x))
+def residuals(params, data):
+    '''
 
-### Should move to tests
-
-def get_mock_segment(radius=30, dtheta=4*np.pi/3,
-                     ellipticity=1.5, noise=1e-4, t_span=90, t_step=3):
-
-    a = radius * ellipticity
-    b = radius / ellipticity
-
-    omega = dtheta/t_span
-
-    phi_x, phi_y, x0, y0 = 0, 0, 0, 0
-
-    params = a, b, omega, phi_x, phi_y, x0, y0
-    ts = np.arange(0, t_span, t_step)
-    t_stamps = ts / t_step
-    t_stamps = t_stamps.astype(np.int)
-
-    x, y = arc_ellipse(ts, params)
-    z = np.ones(x.size)
-    x_err = np.random.normal(scale=noise*radius, size=x.size)
-    y_err = np.random.normal(scale=noise*radius, size=x.size)
-    x += x_err
-    y += y_err
-
-    segment = pd.DataFrame(index=pd.Index(t_stamps, name='t_stamp'),
-                           data=np.vstack([x, y, z, x_err, y_err, ts]).T,
-                           columns=['x', 'y', 'z', 'x_err', 'y_err', 't'])
-    return segment
-
+    '''
+    a, b, phi_x, phi_y, x0, y0 = params
+    x, y = data
+    thetas = np.arctan2(y-y0, x-x0)
+    rhos = np.hypot(x-x0, y-y0)
+    fit_rhos = ellipse_radius(thetas, a, b, phi_x, phi_y)
+    return rhos - fit_rhos
 

cell_tracker/graphics.py
@@ -40,7 +40,7 @@ def _scatter_single_segment(cluster, label, sizes, color, axes=None):
         gof_size = 50 * (gof - gof.min()) / gof.ptp()
 
         ellipticities = ellipsis_df['ellipticity'].astype(np.float)
-        radius = np.exp(ellipsis_df['log_radius'].astype(np.float))
+        radius = ellipsis_df['radius'].astype(np.float)
         rad_size = 50 * (np.log(radius) - np.log(radius.min())) / np.log(radius.max())
         dtheta = ellipsis_df['dtheta'].astype(np.float) * 180 / np.pi
 

cell_tracker/objects.py
@@ -25,7 +25,7 @@ ellipsis_cutoffs = defaults['ellipsis_cutoffs']
 default_metadata = defaults['metadata']
 
 from .analysis import Ellipses
-
+from .utils import continuous_theta
 
 
 class CellCluster:
@@ -190,7 +190,7 @@ class CellCluster:
                                      self.trajs['x_r_pca'])
 
         grouped = self.trajs.groupby(level='label', as_index=False)
-        tmp_trajs = grouped.apply(continuous_theta)
+        tmp_trajs = grouped.apply(continuous_theta_)
         tmp_trajs = tmp_trajs.sortlevel('t_stamp')
         self.trajs = Trajectories(tmp_trajs)
         self.theta_bin_count, self.theta_bins = np.histogram(
@@ -225,12 +225,12 @@ class CellCluster:
             ellipsis.data['good'] =  np.zeros(ellipsis.data.shape[0])
             goods = ellipsis.good_indices(cutoffs)
             if len(goods):
-                ellipsis.data['good'].loc[goods] = 1.
-            ellipsis.data['size'] = size
+                ellipsis_df.loc[goods, 'good'] = 1.
+            ellipsis_df['size'] = size
 
-        data = pd.concat([ellipsis[['gof', 'log_radius',
-                                    'dtheta', 'good', 'size']].astype(np.float)
-                      for ellipsis in self.ellipses.values()]).dropna()
+        data = pd.concat([ellipsis_df[['gof', 'radius',
+                                       'dtheta', 'good', 'size']].astype(np.float)
+                          for ellipsis_df in self.ellipses.values()]).dropna()
         data = data.replace([np.inf, -np.inf], np.nan).dropna()
         data.sort_index(axis=0, inplace=True)
         data.sort_index(axis=1, inplace=True)
@@ -240,7 +240,7 @@ class CellCluster:
         detected_rotations = detected_rotations.swaplevel('label', 't_stamp')
         detected_rotations = detected_rotations.sortlevel(level='label')
         self.detected_rotations = detected_rotations.sortlevel(level='t_stamp')
-        self.trajs['detected_rotations'] = self.detected_rotations
+
 
 def build_iterator(stackio, preprocess=None):
 
@@ -278,7 +278,7 @@ def get_segment_rotations(segment, data):
                                    index=pd.Index(np.arange(t0, t1), name='t_stamp'),
                                    name='detected_rotations')
     try:
-        sub_data = data.loc[label]
+        sub_data = data.xs(label, level='label')
     except KeyError:
         detected_rotations[:] = np.nan
         return detected_rotations
@@ -304,18 +304,12 @@ def evaluate_ellipticity(segment, **kwargs):
     ellipses = Ellipses(segment=segment, **kwargs).data
     return ellipses
 
-
-def continuous_theta(segment):
+def continuous_theta_(segment):
     '''
     Computes a continuous angle from a 2*np.pi periodic one
     '''
-    if segment.shape[0] == 1:
-        segment['dtheta'] = 0
-        return segment
-    theta0 =  segment['theta'].iloc[0]
-    segment['dtheta'] = segment['theta'].diff()
-    segment['dtheta'].iloc[0] = 0
-    segment['dtheta'][segment['dtheta'] > np.pi] -= 2 * np.pi
-    segment['dtheta'][segment['dtheta'] < - np.pi] += 2 * np.pi
-    segment['theta'] = segment['dtheta'].cumsum() + theta0
+    dthetas, thetas = continuous_theta(segment.theta.values)
+    segment['dtheta'] = dthetas
+    segment['theta'] = thetas
     return segment
+

cell_tracker/utils.py
@@ -5,178 +5,22 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 
-from PyQt4 import QtGui
 
 import sys, os
 import logging
 log = logging.getLogger(__name__)
 
+import numpy as np
 import pandas as pd
 
-from sktracker.io import StackIO, ObjectsIO
-from sktracker.io.utils import load_img_list
-from sktracker.trajectories import Trajectories
+def continuous_theta(thetas):
 
-from .. import CellCluster
-from .. import default_metadata
-
-def get_from_excel(default_path='.'):
-    '''
-    This opens a file dialog allowing ot select an excel file containing
-    the tracked data, and returns a :class:`CellCluster` object.
-
-    Paramteters
-    -----------
-
-    default_path : str
-        the path where the file dialog opens
-
-    Returns
-    -------
-
-    cellcluster : a :class:`CellCluster` instance
-         the container class for the tracking
-
-    Notes
-    -----
-
-    The excel file should follow the structure of `excel_trajs_example.xlsx`
-    in the project's `data` directory
-    '''
-
-    ### Open the file dialog
-    data_path, name = get_excel_file(default_path)
-
-    ### Read the data
-    trajs = pd.read_excel(data_path, 0)
-    trajs.set_index(['t_stamp', 'label'],
-                    inplace=True)
-    ### The Trajectories class is a subclass of
-    ### pandas DataFrame
-    trajs = Trajectories(trajs)
-
-    metadata = pd.read_excel(data_path, 1)
-    metadata = {name: value for name, value
-                in zip(metadata['Name'], metadata['Value'])}
-
-    store_path = ''.join(metadata['FileName'].split('.')[:-1]+['.h5'])
-    metadata['FileName'] = os.path.join(
-        os.path.dirname(data_path), metadata['FileName'])
-    store_path = os.path.join(
-        os.path.dirname(data_path), store_path)
-
-    ### The ObjectsIO class
-    objectsio = ObjectsIO(metadata=metadata, store_path=store_path)
-    cellcluster = CellCluster(objectsio=objectsio)
-    cellcluster.trajs = trajs
-    cellcluster.oio['trajs'] = trajs
-    return cellcluster
-
-def get_cluster(default_path='.', metadata=None):
-    '''
-    This opens a file dialog allowing to select the directory for
-    the tracked data, and returns a :class:`CellCluster` object.
-
-    Paramteters
-    -----------
-
-    default_path : str
-        the path where the file dialog opens
-
-    Returns
-    -------
-
-    cellcluster : a :class:`CellCluster` instance
-         the container class for the tracking
-    '''
-
-    if metadata is None:
-        metadata = default_metadata
-    data_path, name = get_dataset(default_path)
-    if data_path is None:
-        return
-
-    stores = [h5file for h5file in os.listdir(data_path)
-              if h5file.endswith('.h5')]
-    if len(stores) == 0:
-        objectsio = None
-    elif len(stores) > 1:
-        log.info('''Multiple '.h5' found, loading metadata from images''')
-        objectsio = None
-    else:
-        store_path = os.path.join(data_path, stores[0])
-        objectsio = ObjectsIO(store_path=store_path)
-
-    image_path_list = load_img_list(data_path)
-    if objectsio is not None:
-        stackio = StackIO(image_path_list=image_path_list,
-                          metadata=objectsio.metadata)
-    else:
-        stackio = StackIO(image_path_list=image_path_list)
-        im0 = stackio.get_tif().asarray()
-        correct_metadata = {'SizeT': len(image_path_list),
-                            'Shape': ((len(image_path_list),)
-                                      + im0.shape)}
-        if len(im0.shape) == 4:
-            correct_metadata['SizeC'] = im0.shape[0]
-            stackio.metadata.update(correct_metadata)
-
-    cellcluster = CellCluster(objectsio=objectsio, stackio=stackio )
-    return cellcluster
-
-def get_dataset(default='.'):
-    '''
-    Opens a directory select dialog
-    '''
-    app = QtGui.QApplication.instance()
-    if not app:
-        app = QtGui.QApplication(sys.argv)
-    out = QtGui.QFileDialog.getExistingDirectory(directory=default)
-    if not len(out):
-        print('''No data loaded''')
-        return None, None
-
-    data_path = str(out)
-    splitted = data_path.split(os.path.sep)
-    name = '{}_{}'.format(splitted[-2], splitted[-1])
-
-    print('Choosen data path: %s' % data_path)
-    return data_path, name
-
-def get_excel_file(default='.'):
-    '''
-    Opens a file select dialog for xlsx files
-    '''
-    app = QtGui.QApplication.instance()
-    if not app:
-        app = QtGui.QApplication(sys.argv)
-    out = QtGui.QFileDialog.getOpenFileName(directory=default,
-                                            caption='Choose an XLSX file',
-                                            filter='*.xlsx')
-    if not len(out):
-        print('''No data loaded''')
-        return None, None
-
-    data_path = str(out)
-    splitted = data_path.split(os.path.sep)
-    name = '{}_{}'.format(splitted[-2], splitted[-1])
-
-    print('Choosen data path: %s' % data_path)
-    return data_path, name
-
-
-def get_name(default=''):
-
-    app = QtGui.QApplication.instance()
-    if not app:
-        app = QtGui.QApplication(sys.argv)
-
-    dialog = QtGui.QInputDialog()
-    name, ok = dialog.getText(dialog, 'Enter tracker name',
-                              'Default is {}:'.format(default))
-    if len(name) and ok:
-        print('Choosen name: %s' % name)
-        return str(name)
-    else:
-        print('Keeping default name {}'.format(default))
-        return default
+    if thetas.shape[0] == 1:
+        dthetas = 0
+        return thetas, dthetas
+    theta0 =  thetas[0]
+    dthetas = np.concatenate(([0], np.diff(thetas)))
+    dthetas[dthetas > np.pi] -= 2 * np.pi
+    dthetas[dthetas < - np.pi] += 2 * np.pi
+    thetas = dthetas.cumsum() + theta0
+    return dthetas, thetas