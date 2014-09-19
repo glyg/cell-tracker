0c686a8d95dacb824360e6d534ab696a49edb4d2
Guillaume Gay <guillaume@mitotic-machine.org>
2014-09-19 15:32:05 +0200

    functional - if not fast - sort ellipses UI

--- cell_tracker/ui/sort_ellipses.py
+++ cell_tracker/ui/sort_ellipses.py
@@ -7,7 +7,6 @@ from __future__ import print_function
 
 from ..graphics import show_4panel_ellipses
 
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,8 +16,6 @@ log = logging.getLogger(__name__)
 
 from sktracker.trajectories import draw
 from ..graphics import show_ellipses
-
-
 
 class EllipsisPicker:
     '''
@@ -36,8 +33,10 @@ class EllipsisPicker:
         self.current_label = self.labels[0]
         self.traj_data = set()
         self.bad_ellipses = []#set()
+        self.bad_lines = []
         self.collected = []
         self.backups = []
+        self.overlays = []
         self.__init_fig()
 
 
@@ -80,22 +79,26 @@ class EllipsisPicker:
                                                             **ellipses_kwargs)
             self.ellipses_lines.append(lines_df)
         self.ellipses_lines = pd.concat(self.ellipses_lines).sortlevel()
+        self.axes[0,0].set_title(self.current_label)
+        plt.draw()
 
     def __call__(self, event):
+
         self.curent_event = event
         if event.inaxes == self.ax_3d: return
-
         if not hasattr(event, 'button'):
             log.info(event.key)
+            print(event.key)
             # if event.key == 'ctrl+z':
             #     if len(self.backups):
             #         self.cluster.trajs = self.backups[-1]
             #         self.__update__()
 
-            if event.key == 'pageup':#in [' ', 'right', 'up']:
-                print('pageup')
+            if event.key in ['pageup', 'ctr+right', 'alt+f']:#in [' ', 'right', 'up']:
+                print('forward + {}'.format(event.key))
                 self.forward()
-            elif event.key == 'pagedown':# in ['left', 'down']:
+            elif event.key in ['pagedown', 'ctr+left', 'alt+b']:# in ['left', 'down']:
+                print('backward + {}'.format(event.key))
                 self.backward()
             elif event.key in ['x', 'X']:
                 self.remove_bad(event)
@@ -109,28 +112,27 @@ class EllipsisPicker:
             elif event.button == 3:
                 if len(self.bad_ellipses):
                     self.unmark_bad(event)
-
             self.canvas.draw()
 
     def mark_bad(self, close_line, event):
         ### Register the data point
-        tls = self.ellipses_lines.loc[event.inaxes, close_line] # time, label, size
+        tls = tuple(self.ellipses_lines.loc[event.inaxes, close_line].values) # time, label, size
         if tls in self.bad_ellipses:
             return
         self.bad_ellipses.append(tls)
-        where = self.ellipses_lines.where(event.inaxes.ellipses_lines == tls).dropna()
+        where = self._reverse_search(tls)
+        self.bad_lines.append(where)
         for ax, line in where.index:
             if ax == self.ax_3d:
                 ### Plots overlay
                 overlay = ax.plot(line.get_data()[0],
                                   line.get_data()[1],
-                                  line.get_data()[2],
                                   'k-', lw=2, alpha=0.8)
             else:
                 overlay = ax.plot(line.get_data()[0],
                                   line.get_data()[1],
                                   'k-', lw=2, alpha=0.8)
-            self.overlays.append(overlay[0])
+            self.overlays.append((ax, overlay[0]))
 
     def iter_axes(self):
         axes = (self.axes[0, 0], self.axes[0, 0], self.axes[0, 0], self.ax_3d)
@@ -140,15 +142,14 @@ class EllipsisPicker:
     def unmark_bad(self, event):
 
         self.bad_ellipses.pop()
+        self.bad_lines.pop()
         ### Plots overlay
-        for ax in reversed(self.iter_axes):
-            overlay = self.overlays.pop()
+        for i in range(4):
+            ax, overlay =  self.overlays.pop()
             try:
                 ax.lines.remove(overlay)
             except ValueError:
                 pass
-            self.bad_lines.pop()
-
 
     def remove_bad(self, event):
         # if not len(self.bad_ellipses):
@@ -157,18 +158,22 @@ class EllipsisPicker:
         #     self.cluster.ellipses.loc[s, t, l]['good'] = 100
         if not len(self.bad_lines):
             return
-        for line in self.bad_lines:
+        for where in self.bad_lines:
+            for ax, line in where.index:
+                try:
+                    ax.lines.remove(line)
+                except ValueError:
+                    pass
+        for ax, overlay in self.overlays:
             try:
-                event.inaxes.lines.remove(line)
-            except ValueError:
-                continue
-        for overlay in self.overlays:
-            try:
-                event.inaxes.lines.remove(overlay)
+                ax.lines.remove(overlay)
             except ValueError:
                 continue
         self.bad_lines = []
         self.overlays = []
+        for tls in self.bad_ellipses:
+            self.cluster.ellipses.loc[tls, 'good'] = -1
+
         plt.draw()
 
     def forward(self):
@@ -176,13 +181,16 @@ class EllipsisPicker:
         if self.current_label != self.labels[-1]:
             self.current_label = self.labels[self.labels.index(self.current_label)+1]
             self.__update__()
-            self.canvas.draw()
+        else:
+            self.axes[0, 0].set_title('No more label before')
 
     def backward(self):
 
         if self.current_label != self.labels[0]:
             self.current_label = self.labels[self.labels.index(self.current_label)-1]
             self.__update__()
+        else:
+            self.axes[0, 0].set_title('No more label after')
 
     def __update__(self):
         print('update')
@@ -196,15 +204,6 @@ class EllipsisPicker:
         for ax in self.axes.ravel():
             self.traj_data.update(set(ax.lines))
         self.show_pickable_ellipses()
-        # show_4panel_ellipses(self.cluster, self.current_label,
-        #                      sizes=self.sizes,  cutoffs=None,
-        #                      method='polar',
-        #                      scatter_kw={}, line_kw={},
-        #                      ellipsis_kw={},
-        #                      show_centers=False,
-        #                      savefile=None, axes=None, ax_3d=None)
-
-
 
     def _closest_line(self, event):
 
@@ -223,3 +222,11 @@ class EllipsisPicker:
                 min_dist = min(dist)
                 closest = line
         return closest
+
+    def _reverse_search(self, tls):
+
+        t, l, s = tls
+        ts = self.ellipses_lines['t_stamp'] == t
+        ls = self.ellipses_lines['label'] == l
+        ss = self.ellipses_lines['size'] == s
+        return self.ellipses_lines[ts & ls & ss]
